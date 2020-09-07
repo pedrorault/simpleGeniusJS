edc5ba2b6ac9b380839e6fa9a7ea9e4021b12f43
Pedro R Costa <pedrorault@gmail.com>
2020-09-07 19:51:34 -0300

    added circle in the middle and the text go

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-function hi(){
-    console.log("Hi")    
-}
-
 function newParagraph(someString){
     p = document.createElement("p")
     p.textContent = someString || "Parâmetro vazio, para"
     return p
 }
-
-function createSVG(){
-    var svg = document.createElementNS("http://www.w3.org/2000/svg","svg")
-    return svg
-
+function chooseColor(list){
+    let colors = ["green","red","yellow","blue"]
+    let randomColor = colors[Math.floor(Math.random() * colors.length)]
+    let currentList = list || []
+    currentList.push(randomColor)
+    return currentList
 }
-function onLoad(){        
-    // document.body.appendChild(createSVG())
-    let c = document.getElementById("svg")
+
+function onLoad(){  
+    let svgObj = document.getElementById("svg").contentDocument;
+    let colorMap = new Map()
+    colorMap.set("green" , "lightgreen")
+    colorMap.set("red" , "lightcoral")
+    colorMap.set("yellow" , "Khaki") 
+    colorMap.set("blue" , "lightblue")
+
+    for (let [key,value] of colorMap){
+        let obj = svgObj.getElementById(key)
+        obj.style.fill = key
+        obj.addEventListener("mouseover", () => obj.style.fill = value)
+        obj.addEventListener("mouseout", () => obj.style.fill = key)
+    }   
+
 }
